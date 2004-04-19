9ac1acebfa1a5b3df2ae1e1ba4b4254c84a9b767
mailund <mailund@95538ddd-8cd3-0310-8990-a11293a2e85a>
2004-04-19 13:18:01 +0000

    some float/int/string precision bugfixes

--- gui/add_marker_impl.cc
+++ gui/add_marker_impl.cc
@@ -8,6 +8,15 @@
 
 #include "baps_float_spin_box.hh"
 
+
+// FIXME: it is hardwired througout this module, that the positions
+// are 1/1000 of the integer positions we operate on.  Working on the
+// floats directly might cause rounding errors when converting to and
+// from inters, doubles, and strings, which is why we only operate on
+// the integers.
+
+
+
 /* 
  *  Constructs a AddMarkerImpl which is a child of 'parent', with the 
  *  name 'name' and widget flags set to 'f' 
@@ -26,17 +35,16 @@ AddMarkerImpl::~AddMarkerImpl()
     // no need to delete child widgets, Qt does it all for us
 }
 
-bool AddMarkerImpl::check_position(double pos) const
-{
-  // since we know that the gui will not allow us to input positions
-  // outside the interval [0.0,1.0), we do not check for this.
 
+bool AddMarkerImpl::check_position(int pos) const
+{
   // FIXME: if this turns out to be too slow, use binary search -- we
   // know the positions are sorted.
   int no_rows = _marker_table->numRows();
   for (int i = 0; i < no_rows; ++i)
     {
-      double row_pos = _marker_table->text(i,0).toDouble();
+      // text field == "0.xxx" where xxx is the int we want
+      int row_pos = _marker_table->text(i,0).mid(2).toInt();
       if (row_pos == pos)
 	{
 	  // the pos is not allowed; it is already taken
@@ -51,19 +59,15 @@ bool AddMarkerImpl::check_position(double pos) const
 }
 
 
-// FIXME: this scale factor is hard-wired for now; perhaps it
-// shouldn't be...
-static const int POSITION_SCALE_FACTOR = 1000;
-
 
 void AddMarkerImpl::add_trait()
 {
-  double pos = _trait_pos->text().toDouble();
+  int pos = _trait_pos->value();
   if (!check_position(pos)) return;
 
   _marker_table->insertRows(0);
 
-  _marker_table->setText(0,0,_trait_pos->text());
+  _marker_table->setText(0,0,QString("").sprintf("0.%03d",pos));
   _marker_table->setText(0,1,"trait");
   _marker_table->setText(0,2,_trait_low_freq->text());
   _marker_table->setText(0,3,_trait_high_freq->text());
@@ -76,12 +80,12 @@ void AddMarkerImpl::add_trait()
 
 void AddMarkerImpl::add_snp()
 {
-  double pos = _snp_pos->text().toDouble();
+  int pos = _snp_pos->value();
   if (!check_position(pos)) return;
 
   _marker_table->insertRows(0);
 
-  _marker_table->setText(0,0,_snp_pos->text());
+  _marker_table->setText(0,0,QString("").sprintf("0.%03d",pos));
   _marker_table->setText(0,1,"snp");
   _marker_table->setText(0,2,_snp_low_freq->text());
   _marker_table->setText(0,3,_snp_high_freq->text());
@@ -94,12 +98,12 @@ void AddMarkerImpl::add_snp()
 
 void AddMarkerImpl::add_ms()
 {
-  double pos = _ms_pos->text().toDouble();
+  int pos = _ms_pos->value();
   if (!check_position(pos)) return;
 
   _marker_table->insertRows(0);
 
-  _marker_table->setText(0,0,_ms_pos->text());
+  _marker_table->setText(0,0,QString("").sprintf("0.%03d",pos));
   _marker_table->setText(0,1,"ms");
   _marker_table->setText(0,2,"");
   _marker_table->setText(0,3,"");
@@ -112,29 +116,23 @@ void AddMarkerImpl::add_ms()
 
 void AddMarkerImpl::next_pos_changed(int pos)
 {
-  double real_pos = double(pos) / POSITION_SCALE_FACTOR;
-  _next_pos->setText(QString("%1").arg(real_pos));
-
-  // WARNING: this only works 'cause the integer positions in the slider
-  // and the spin-boxes have the same interpretation
+  _next_pos->setText(QString("").sprintf("0.%03d",pos));
   _trait_pos->setValue(pos);
   _snp_pos  ->setValue(pos);
   _ms_pos   ->setValue(pos);
 }
 
-int AddMarkerImpl::update_next_position(double pos)
+
+int AddMarkerImpl::update_next_position(int pos)
 {
-  double cur_next_pos = _next_pos->text().toDouble();
-  if (pos != cur_next_pos) return int(cur_next_pos*POSITION_SCALE_FACTOR);
+  int cur_next_pos = _next_pos_slider->value();
+  if (pos != cur_next_pos) return cur_next_pos;
 
-  double step = _next_pos_step->text().toDouble();
-
-  int int_cur_next_pos = int(POSITION_SCALE_FACTOR*cur_next_pos);
-  int int_step         = int(POSITION_SCALE_FACTOR*step);
-  int int_pos = int_cur_next_pos + int_step;
+  int step = _next_pos_step->value();
+  int next_pos = cur_next_pos + step;
 
   // move position -- this automatically updates all that depends on it.
-  _next_pos_slider->setValue(int_pos);
+  _next_pos_slider->setValue(next_pos);
 
-  return int_pos;
+  return next_pos;
 }
--- gui/add_marker_impl.hh
+++ gui/add_marker_impl.hh
@@ -9,14 +9,17 @@ class AddMarkerImpl : public AddMarkerForm
   Q_OBJECT
   QTable *_marker_table;
 
+  // NB: the positions below are in the integer coordinates -- to get
+  // the "real" coordinates, divide by 1000
+
   // returns true if it is allowed to add the position, false
   // otherwise.  Shows feedback to the user.
-  bool check_position(double pos) const;
+  bool check_position(int pos) const;
 
   // if pos is the current "next position", increase next position,
-  // otherwise do nothing.  Return the new (integer) position pos
-  // should be set to.
-  int update_next_position(double pos);
+  // otherwise do nothing.  Return the new position pos should be set
+  // to.
+  int update_next_position(int pos);
 
 public:
   AddMarkerImpl( QTable *marker_table,
